9aceb2a1ce1fcc96e25410ce468eab75af3bdb77
Oliver Otchere <oliverotchere4@gmail.com>
2023-08-15 19:23:38 +0000

    fixed build

--- pages/facebook.tsx
+++ pages/facebook.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import queryString from "query-string";
 import FacebookLogin from "@greatsumini/react-facebook-login";
+import { GoogleLogin } from "react-google-login";
 
 const Facebook = () => {
   const stringifiedParams = queryString.stringify({
@@ -11,6 +12,32 @@ const Facebook = () => {
     auth_type: "rerequest",
     display: "popup",
   });
+
+  const responseGoogle = async (response: any) => {
+    console.log(response);
+    // const url = "/api/google-login";
+
+    // const res = await fetch(url, {
+    //   method: "POST",
+    //   headers: {
+    //     "content-type": "application/json",
+    //   },
+    //   body: JSON.stringify({
+    //     token: response.tokenId,
+    //   }),
+    // });
+
+    // if (!res.ok) return toast.error("Something went wrong... please try again");
+
+    // const currentUser = await checkUserLoggedIn();
+    // if (currentUser) {
+    //   setUser(() => currentUser?.full_name);
+
+    //   dispatch(registerUser(currentUser?.full_name));
+    // }
+
+    // router.push("/");
+  };
 
   const facebookLoginUrl = `https://www.facebook.com/v4.0/dialog/oauth?${stringifiedParams}`;
   return (
@@ -26,6 +53,28 @@ const Facebook = () => {
         onProfileSuccess={(response) => {
           console.log("Get Profile Success!", response);
         }}
+      />
+
+      <GoogleLogin
+        clientId="990750877166-rompnb60e7kqho9a4a2p2khc2dj26ebc.apps.googleusercontent.com"
+        // responseType="code"
+        render={(renderProps) => (
+          <button
+            onClick={renderProps.onClick}
+            disabled={renderProps.disabled}
+            className="py-3 px-4 lg:px-7 shadow-lg w-full outline-none mx-auto rounded-md flex justify-center items-center text-gray-50 bg-red-400"
+          >
+            <>
+              <span className="ml-2 font-semibold lg:font-medium">
+                Log In with Google
+              </span>
+            </>
+          </button>
+        )}
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        // cookiePolicy={''}
       />
     </div>
   );
